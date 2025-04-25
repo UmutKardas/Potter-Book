3542d7b3e9a23bdba78373384a0b47bf50d70aff
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2025-04-25 20:47:18 +0300

    Add AppSvgPaths class with SVG asset paths for Hogwarts houses

---
+++ potter_book/lib/constants/app_svg_paths.dart
@@ -0,0 +1,8 @@
+class AppSvgPaths {
+  static const String root = 'assets/svg';
+
+  static const String gryffindor = '$root/Gryffindor.svg';
+  static const String hufflepuff = '$root/Hufflepuff.svg';
+  static const String ravenclaw = '$root/Ravenclaw.svg';
+  static const String slytherin = '$root/Slytherin.svg';
+}
